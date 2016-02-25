300140b4f70eace5111d50b7257e362f44d1713c
Debidatta Dwibedi <debi.me@gmail.com>
2016-02-24 20:26:08 -0500

    added python param str with exception catching

--- include/caffe/python_layer.hpp
+++ include/caffe/python_layer.hpp
@@ -14,26 +14,33 @@ template <typename Dtype>
 class PythonLayer : public Layer<Dtype> {
  public:
   PythonLayer(PyObject* self, const LayerParameter& param)
-      : Layer<Dtype>(param), self_(self) { }
+      : Layer<Dtype>(param), self_(bp::handle<>(bp::borrowed(self))) { }
 
   virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
+    // Disallow PythonLayer in MultiGPU training stage, due to GIL issues
+    // Details: https://github.com/BVLC/caffe/issues/2936
     try {
-      bp::call_method<bp::object>(self_, "setup", bottom, top);
-    } catch (bp::error_already_set) {
+    self_.attr("param_str") = bp::str(
+        this->layer_param_.python_param().param_str());
+    }catch (bp::error_already_set) {
+      PyErr_Print();
+      throw;
+    }
+    try {
+    self_.attr("setup")(bottom, top);
+    }catch (bp::error_already_set) {
       PyErr_Print();
       throw;
     }
   }
-
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-    try {
-      bp::call_method<bp::object>(self_, "reshape", bottom, top);
-    } catch (bp::error_already_set) {
-      PyErr_Print();
-      throw;
-    }
+    self_.attr("reshape")(bottom, top);
+  }
+
+  virtual inline bool ShareInParallel() const {
+    return this->layer_param_.python_param().share_in_parallel();
   }
 
   virtual inline const char* type() const { return "Python"; }
@@ -41,26 +48,15 @@ class PythonLayer : public Layer<Dtype> {
  protected:
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-    try {
-      bp::call_method<bp::object>(self_, "forward", bottom, top);
-    } catch (bp::error_already_set) {
-      PyErr_Print();
-      throw;
-    }
+    self_.attr("forward")(bottom, top);
   }
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-    try {
-      bp::call_method<bp::object>(self_, "backward", top, propagate_down,
-          bottom);
-    } catch (bp::error_already_set) {
-      PyErr_Print();
-      throw;
-    }
+    self_.attr("backward")(top, propagate_down, bottom);
   }
 
  private:
-  PyObject* self_;
+  bp::object self_;
 };
 
 }  // namespace caffe
