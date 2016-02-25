a1178d3955efbe476fbe59296297d5b4e17c38d0
Debidatta Dwibedi <debi.me@gmail.com>
2016-02-24 20:24:46 -0500

    added label_dropout cpp layer

--- include/caffe/common_layers.hpp
+++ include/caffe/common_layers.hpp
@@ -442,6 +442,39 @@ class NormalizeLayer : public Layer<Dtype> {
   Dtype eps_;
 };
 
+template <typename Dtype>
+class LabelDropoutLayer : public Layer<Dtype> {
+ public:
+  explicit LabelDropoutLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      vector<Blob<Dtype>*>* top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "LabelDropout"; }
+  virtual inline int ExactNumBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  /// @brief Not implemented (non-differentiable function)
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  //virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+  //    vector<Blob<Dtype>*>* top);
+  //virtual Dtype Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+  //    vector<Blob<Dtype>*>* top);
+  //virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+  //    const bool propagate_down, vector<Blob<Dtype>*>* bottom);
+  //virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+  //    const bool propagate_down, vector<Blob<Dtype>*>* bottom);
+  void set_mask(const vector<Blob<Dtype>*>& bottom);
+
+  Blob<Dtype> mask_;
+
+};
 /**
  * @brief Count the prediction and ground truth statistics for each datum.
  *
